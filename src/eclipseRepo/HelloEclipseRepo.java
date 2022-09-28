eb3f3fb610c87b03c0c8deb7cc5a440c3ec2f490
Marcelino Rutea López <marcelino.rutea@civica-soft.com>
2022-09-28 10:12:52 +0200

    merged with remote

--- src/eclipseRepo/HelloEclipseRepo.java
+++ src/eclipseRepo/HelloEclipseRepo.java
@@ -4,6 +4,8 @@ public class HelloEclipseRepo {
 
 	public static void main(String[] args) {
 		System.out.println("HelloEclipseRepo!");
+		System.out.println();
+		System.out.println("New line");
 	}
 
 }
