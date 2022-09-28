08e0206aefd617887a3e82b81ecc805809285325
Marcelino Rutea López <marcelino.rutea@civica-soft.com>
2022-09-28 11:21:29 +0200

    delete line

--- src/eclipseRepo/HelloEclipseRepo.java
+++ src/eclipseRepo/HelloEclipseRepo.java
@@ -4,7 +4,6 @@ public class HelloEclipseRepo {
 
 	public static void main(String[] args) {
 		System.out.println("HelloEclipseRepo!");
-		System.out.println();
 		System.out.println("New line");
 	}
 
